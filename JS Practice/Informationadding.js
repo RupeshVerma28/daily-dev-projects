2492631ce66c2f3ee8edc20703a904d3d4901602
RupeshVerma28 <rupeshvermapc@gmail.com>
2026-06-20 20:38:54 +0530

    Information.js : Logic Building Exersice

---
+++ JS Practice/Informationadding.js
@@ -0,0 +1,35 @@
+// First Version : Complex to add more information
+const information = {
+  name: ["Jack", "Paul", "Thunder"],
+  role: ["Developer", "Designer", "Security Analyst"],
+  age: [21, 22, 28],
+};
+/*console.log("Introduction To Our Respectful Team");
+
+console.log(
+  `Name: ${information.name[0]} , Role: ${information.role[0]}, Age:${information.age[0]}`,
+);
+console.log(
+  `Name: ${information.name[1]} , Role: ${information.role[1]}, Age:${information.age[1]}`,
+);
+console.log(
+  `Name: ${information.name[2]} , Role: ${information.role[2]}, Age:${information.age[2]}`,
+);
+ */
+//Fun Time 😁 : Role Exchange
+console.log(
+  `Name: ${information.name[0]} , Role: ${information.role[2]}, Age:${information.age[1]}`,
+);
+
+// Second Version: Better Version from Chat GPT inspiration (Easy to add  Unlimited Information )
+
+/*
+function member(name, role, age) {
+  console.log(`Name: ${name} , Role: ${role}, Age:${age}`);
+}
+console.log("|| OUR RESPECTFUL TEAM ||");
+
+member("Jack", "Developer", 28);
+member("Paul", "Designer", 24);
+member("Thunder", "Security Analyst", 21);
+*/
